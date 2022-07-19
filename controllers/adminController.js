eb8acfc821cf67a99b7de141067d85ce2768ccd8
Ahmedkarm310501 <84907048+Ahmedkarm310501@users.noreply.github.com>
2022-07-19 20:15:25 +0200

    add admin fuctionlties

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,10 +1,18 @@
 const { validationResult } = require("express-validator");
 const path = require("path");
 const fs = require("fs");
-
+const bcrypt = require("bcrypt");
 const Product = require("../models/product");
 const Category = require("../models/category");
 const User = require("../models/user");
+const Order = require("../models/order");
+
+const delete_image = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => {
+    if (err) console.log(err);
+  });
+};
 
 exports.addProduct = async (req, res, next) => {
   const errors = validationResult(req);
@@ -92,9 +100,178 @@ exports.deleteProduct = async (req, res, next) => {
   }
 };
 
-const delete_image = (filePath) => {
-  filePath = path.join(__dirname, "..", filePath);
-  fs.unlink(filePath, (err) => {
-    if (err) console.log(err);
-  });
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.findAll();
+    const newUsers = users.map((user) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        status: user.status == 0 ? "suspensed" : "active",
+        photo: user.photo,
+      };
+    });
+    res.status(200).json({
+      message: "Users fetched successfully",
+      users: newUsers,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.addUser = async (req, res, next) => {
+  console.log(req.body);
+  const { name, email, password, dateOfBirth, isAdmin, status } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email: email } });
+    if (user) {
+      const error = new Error("User already exists");
+      error.statusCode = 401;
+      throw error;
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await User.create({
+      email: email,
+      name: name,
+      password: hashedPassword,
+      dateOfBirth: new Date(dateOfBirth),
+      isAdmin: isAdmin,
+      status: status,
+    });
+    await newUser.createCart({});
+    await newUser.createFavourite({});
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    throw err;
+  }
+};
+
+exports.getUser = async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId);
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+    user.password = undefined;
+    res.status(200).json({
+      message: "User fetched successfully",
+      user: user,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateUser = async (req, res, next) => {
+  console.log(req.body);
+  const { name, email, password, dateOfBirth, isAdmin, status } = req.body;
+  console.log(name, email, password, dateOfBirth, isAdmin, status);
+  try {
+    const user = await User.findByPk(req.params.userId);
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+    user.name = name;
+    user.email = email;
+    user.dateOfBirth = dateOfBirth;
+    user.isAdmin = isAdmin;
+    user.status = status;
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 12);
+      user.password = hashedPassword;
+    }
+    await user.save();
+    res.status(200).json({
+      message: "User updated successfully",
+      user: user,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getAllOrders = async (req, res, next) => {
+  try {
+    const orders = await Order.findAll({
+      include: [{ model: User }],
+    });
+    const newOrders = orders.map((order) => {
+      return {
+        id: order.id,
+        userName: order.user.name,
+        userEmail: order.user.email,
+        userPhoto: order.user.photo,
+        totalPrice: order.totalPrice,
+        orderStatus: order.status,
+      };
+    });
+    res.status(200).json({
+      message: "Orders fetched successfully",
+      orders: newOrders,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.confirmOrder = async (req, res, next) => {
+  try {
+    const order = await Order.findByPk(req.params.orderId);
+    if (!order) {
+      const error = new Error("Order not found");
+      error.statusCode = 404;
+      throw error;
+    }
+    order.status = true;
+    await order.save();
+    res.status(200).json({
+      message: "Order confirmed successfully",
+      order: order,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.dashboard = async (req, res, next) => {
+  try {
+    const orders = await Order.findAll();
+    let pendingPrice = 0;
+    let confirmedPrice = 0;
+    let totalPrice = 0;
+    orders.forEach((order) => {
+      if (order.status == false) pendingPrice += order.totalPrice;
+      if (order.status == true) confirmedPrice += order.totalPrice;
+      totalPrice += order.totalPrice;
+    });
+
+    const users = await User.findAll({
+      order: [["id", "ASC"]], //
+      limit: 2,
+    });
+
+    const lastOrders = await Order.findAll({
+      order: [["id", "ASC"]],
+      limit: 2,
+      include: [{ model: User }],
+    });
+
+    res.status(200).json({
+      message: "Dashboard fetched successfully",
+      pendingPrice: pendingPrice,
+      confirmedPrice: confirmedPrice,
+      totalPrice: totalPrice,
+      lastUsers: users,
+      lastOrders: lastOrders,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
